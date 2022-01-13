68ef5500197140d23db3553b5b865a87c2b30b35
Michael Orvin <morvin1212@gmail.com>
2022-01-13 11:13:30 -0700

    wip

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const cohort1 = process.argv[2]
-const user = process.env.USER
+// to inject the config vars inside the .env
+require('dotenv').config()
 
-if (cohort1 === 'web-49') {
-  console.log(`${cohort1} is the best forever!`)
-} else {
-  console.log(`booooh cohort ${cohort1}`)
-}
+// const cohort1 = process.argv[2]
+// const user = process.env.USER
+// const shell = process.env.SHELL
 
-console.log(`the user is ${user}`)
+// if (cohort1 === 'web-49') {
+//   console.log(`${cohort1} is the best forever!`)
+// } else {
+//   console.log(`booooh cohort ${cohort1}`)
+// }
+
+// console.log(`the user is ${user}`)
+// console.log(`the shell is ${shell}`)
+
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'hey there'})
+})
+
+// heroku wants to set its own port
+const port = process.env.PORT || 9000
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
